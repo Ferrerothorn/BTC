396dcad8820b54f45052d73436fd2ffaac4a11de
Stephen Dolman <sdolman@sabio.co.uk>
2017-08-22 14:07:30 +0100

    Removing as much code as possible from runner script to allow greater test coverage.

--- src/swissTournamentRunner/RunTourney.java
+++ src/swissTournamentRunner/RunTourney.java
@@ -5,42 +5,24 @@ public class RunTourney {
 	static Tournament tourney = new Tournament();
 
 	public static void main(String[] args) {
-
 		GUI gui = new GUI(tourney);
-		tourney.setGUI(gui);
+		tourney.setGUI(gui); 
 		GUI.createAndShowGUI(true);
 
 		showCredits();
 		tourney.signUpPlayers();
 		tourney.allParticipantsIn = true;
 
-		while (tourney.roundNumber <= tourney.getNumberOfRounds() && tourney.players.size() > 1) {
-			GUI.wipePane();
-			tourney.shufflePlayers();
-			tourney.updateParticipantStats();
+		tourney.run();
 
-			tourney.sortRankings();
-			GUI.postResultsString(GUI.generateInDepthRankings(tourney.players));
-			tourney.generatePairings(0);
-			tourney.pollForResults();
-			if (tourney.isElimination) {
-				tourney.elimination();
-			} else {
-				tourney.roundNumber++;
-			}
-		}
 		TntFileManager.saveTournament(tourney);
 		GUI.wipePane();
-		GUI.postString("FINAL STANDINGS");
-		tourney.updateParticipantStats();
-		GUI.postString(GUI.generateInDepthRankings(tourney.players));
-		GUI.postString(tourney.postTournamentAwards());
 		tourney.postTourneyProcessing();
 	}
 
 	private static void showCredits() {
 		GUI.postString("Welcome to B-T-C, the Swiss Tournament Bracket Organiser!");
-		GUI.postString("(Version 10.1.2 - Made by Steve Dolman)");
+		GUI.postString("(Version 10.5 - Made by Steve Dolman)");
 		GUI.postString("Shoutout to Rachel Dolman and Darren Macey for help in testing and debug - #NAK4LYF!");
 	}
 }
