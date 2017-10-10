44e9e13ebf6864db06d3e6fcdd1122e80641699c
Stephen Dolman <sdolman@sabio.co.uk>
2017-10-10 17:10:54 +0100

    Registred players count now legislates for players in a preseeded game.

--- src/swissTournamentRunner/Tournament.java
+++ src/swissTournamentRunner/Tournament.java
@@ -74,10 +74,8 @@ public class Tournament {
 
 	public void postListOfConfirmedSignups() {
 		Collections.sort(players);
-		int totalNumberOfPlayers = players.size();
-		if (allParticipantsIn) {
-			totalNumberOfPlayers += currentBattles.size() * 2;
-		}
+		int totalNumberOfPlayers = players.size() + currentBattles.size() * 2;
+		
 		String post = "-=-=-Registered: " + totalNumberOfPlayers + " players. -=-=-" + "\n";
 		for (int i = 1; i <= players.size(); i++) {
 			post += "" + i + ") " + players.get(i - 1).getName() + "\n";
