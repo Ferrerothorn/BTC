9b1993f493c6393e958e502d74fe24ba24d2fcb6
Ferrerothorn <stephendolman2112@gmail.com>
2018-09-17 15:11:04 +0100

    For the time being, no need for arraylist here.

--- src/swissTournamentRunner/TntFileManager.java
+++ src/swissTournamentRunner/TntFileManager.java
@@ -7,7 +7,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
 
 public class TntFileManager {
 
