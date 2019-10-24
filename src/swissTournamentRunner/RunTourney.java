b2c4e09b37aa6903031b239545a34d286f83a2f5
Steve Dolman <stephendolman2112@gmail.com>
2019-10-24 15:09:51 +0100

    Dropped players don't show up in results any more. Fixed spacing on results panel.

--- src/swissTournamentRunner/RunTourney.java
+++ src/swissTournamentRunner/RunTourney.java
@@ -17,7 +17,7 @@ public class RunTourney {
 
 	private static void showCredits() {
 		GUI.postString("Welcome to FFTCG BTC, the Final Fantasy TCG Swiss Tournament Bracket Organiser!");
-		GUI.postString("(Summer 2019 Edition - Made by Steve Dolman)");
-		GUI.postString("Shoutout to Sera-Jane Cooley, Rachel Dolman, and Darren Macey for help in testing and debug!");
+		GUI.postString("(Autumn 2019 Edition - Made by Steve Dolman)");
+		GUI.postString("Shoutout to Tokyo Toys Glasgow, Slice and Dice Nottingham, and Nerd Shak Wigan for help in testing and debug!");
 	}
 }
